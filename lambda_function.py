f861bcc9b17b35224380675bc77e2ee93804c0d5
Gabriela Ribeiro <g.sribeiro@hotmail.com>
2023-11-06 20:56:39 -0300

    Test 1

--- lambda_function.py
+++ lambda_function.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
     width=event['width']
     
     area = calculate_area(length, width)
-    print(f"The area is {area}")
+    print(f"The area is testeeeeeeeeee {area}")
         
     logger.info(f"CloudWatch logs group: {context.log_group_name}")
     
